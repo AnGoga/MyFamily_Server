e0dc60e4f9b302faf9dcb1c6dbe759ef2a1fdc1c
AnGoga <Anogeorgy05@gmail.com>
2021-12-17 16:49:07 +0300

    ApiGateway service + DiscoveryService + eureka integration

---
+++ DiscoveryService/src/test/kotlin/com/angogasapps/discoveryservice/DiscoveryServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.angogasapps.discoveryservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
