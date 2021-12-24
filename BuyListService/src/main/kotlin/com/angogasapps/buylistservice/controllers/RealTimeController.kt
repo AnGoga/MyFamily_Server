3052e93d5ff68f9b880641f19a620d11fdfdefbf
AnGoga <Anogeorgy05@gmail.com>
2021-12-24 17:17:07 +0300

    send all buy list on first subscribe

--- BuyListService/src/main/kotlin/com/angogasapps/buylistservice/controllers/RealTimeController.kt
+++ BuyListService/src/main/kotlin/com/angogasapps/buylistservice/controllers/RealTimeController.kt
@@ -1,21 +1,30 @@
 package com.angogasapps.buylistservice.controllers
 
+import com.angogasapps.buylistservice.entities.BuyList
+import com.angogasapps.buylistservice.services.BuyListService
 import com.angogasapps.buylistservice.values.PATH_BUY_LIST_LISTENER
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.messaging.handler.annotation.SendTo
 import org.springframework.messaging.simp.SimpMessagingTemplate
+import org.springframework.messaging.simp.annotation.SubscribeMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 
 @RestController
 class RealTimeController {
     @Autowired
-    private lateinit var messagingTemplate: SimpMessagingTemplate
+    private lateinit var buyListService: BuyListService
 
     @MessageMapping("/post")
     @SendTo("$PATH_BUY_LIST_LISTENER/{family_id}")
     fun eho(string: String): String {
         return string
+    }
+
+    @SubscribeMapping("/topic/buy_lists/changes/{familyId}")
+    fun onSubscribe(@PathVariable familyId: String): List<BuyList> {
+        return buyListService.getAllBuyLists(familyId)
     }
 }
